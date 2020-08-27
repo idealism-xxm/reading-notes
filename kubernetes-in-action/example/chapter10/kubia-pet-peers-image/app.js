42c08fff411f94b733eff74cf4bfb8ec8f1c0eff
idealism-xxm <biaochekuangmo@yeah.net>
2020-08-27 23:57:42 +0800

    docs: 《Kubernetes in Action》- StatefulSet: 部署有状态的多副本应用

---
+++ kubernetes-in-action/example/chapter10/kubia-pet-peers-image/app.js
@@ -0,0 +1,78 @@
+const http = require('http');
+const os = require('os');
+const fs = require('fs');
+const dns = require('dns');
+
+const dataFile = "/var/data/kubia.txt";
+const serviceName = "kubia.default.svc.cluster.local";
+const port = 8080;
+
+
+function fileExists(file) {
+    try {
+        fs.statSync(file);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+function httpGet(reqOptions, callback) {
+    return http.get(reqOptions, function(response) {
+        var body = '';
+        response.on('data', function(d) { body += d; });
+        response.on('end', function() { callback(body); });
+    }).on('error', function(e) {
+        callback("Error: " + e.message);
+    });
+}
+
+var handler = function(request, response) {
+    if (request.method == 'POST') {
+        var file = fs.createWriteStream(dataFile);
+        file.on('open', function (fd) {
+            request.pipe(file);
+            response.writeHead(200);
+            response.end("Data stored on pod " + os.hostname() + "\n");
+        });
+    } else {
+        response.writeHead(200);
+        if (request.url == '/data') {
+            var data = fileExists(dataFile) ? fs.readFileSync(dataFile, 'utf8') : "No data posted yet";
+            response.end(data);
+        } else {
+            response.write("You've hit " + os.hostname() + "\n");
+            response.write("Data stored in the cluster:\n");
+            // 通过 DNS 查询获取 SRV 记录
+            dns.resolveSrv(serviceName, function (err, addresses) {
+                if (err) {
+                    response.end("Could not look up DNS SRV records: " + err);
+                    return;
+                }
+                var numResponses = 0;
+                if (addresses.length == 0) {
+                    response.end("No peers discovered.");
+                } else {
+                    // 与 SRV 记录对应的每个 Pod 通信获取其数据
+                    addresses.forEach(function (item) {
+                        var requestOptions = {
+                            host: item.name,
+                            port: port,
+                            path: '/data'
+                        };
+                        httpGet(requestOptions, function (returnedData) {
+                            numResponses++;
+                            response.write("- " + item.name + ": " + returnedData + "\n");
+                            if (numResponses == addresses.length) {
+                                response.end();
+                            }
+                        });
+                    });
+                }
+            });
+        }
+    }
+};
+
+var www = http.createServer(handler);
+www.listen(port);
